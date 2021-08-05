c1fbe8ce0b8e593ff26672949ffccd43db2e1285
rochdev <roch.devost@datadoghq.com>
2021-08-05 10:59:06 -0400

    code cleanup

--- bench.js
+++
@@ -1,7 +0,0 @@
-const { ldexp: old } = require('@stdlib/math-base-special-ldexp')
-const { frexp, ldexp } = require('./dist/ddsketch/math')
-
-for (let i = 0; i < 2e7; i++) {
-    // frexp(0.02336)
-    ldexp(0.5, 3)
-}
