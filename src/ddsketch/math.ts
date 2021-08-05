980d0072f2604b96631f33a2bf49d92ce3e8530a
rochdev <roch.devost@datadoghq.com>
2021-08-05 09:20:47 -0400

    code cleanup

--- src/ddsketch/math.ts
+++ src/ddsketch/math.ts
@@ -10,34 +10,28 @@
  * and an integer power of two.
  */
 export function frexp(value: number): [number, number] {
-    value = Number(value);
+    if (value === 0 || !Number.isFinite(value)) return [value, 0];
 
-    const result: [number, number] = [value, 0];
+    const absValue = Math.abs(value);
 
-    if (value !== 0 && Number.isFinite(value)) {
-        const absArg = Math.abs(value);
-        let exp = Math.max(-1023, Math.floor(Math.log2(absArg)) + 1);
-        let x = absArg * Math.pow(2, -exp);
-        // These while loops compensate for rounding errors that sometimes occur
-        // because of ECMAScript's Math.log2's undefined precision and also
-        // works around the issue of Math.pow(2, -exp) === Infinity when
-        // exp <= -1024
-        while (x < 0.5) {
-            x *= 2;
-            exp--;
-        }
-        while (x >= 1) {
-            x *= 0.5;
-            exp++;
-        }
-        if (value < 0) {
-            x = -x;
-        }
-        result[0] = x;
-        result[1] = exp;
+    let exponent = Math.max(-1023, Math.floor(Math.log2(absValue)) + 1);
+    let mantissa = absValue * Math.pow(2, -exponent);
+
+    while (mantissa < 0.5) {
+        mantissa *= 2;
+        exponent--;
     }
 
-    return result;
+    while (mantissa >= 1) {
+        mantissa *= 0.5;
+        exponent++;
+    }
+
+    if (value < 0) {
+        mantissa = -mantissa;
+    }
+
+    return [mantissa, exponent]
 }
 
 /**
@@ -45,10 +39,13 @@ export function frexp(value: number): [number, number] {
  * two; i.e., x = frac * 2^exp.
  */
 export function ldexp(mantissa: number, exponent: number): number {
-    const steps = Math.min(3, Math.ceil(Math.abs(exponent) / 1023));
+    const iterations = Math.min(3, Math.ceil(Math.abs(exponent) / 1023));
+
     let result = mantissa;
-    for (let i = 0; i < steps; i++) {
-        result *= Math.pow(2, Math.floor((exponent + i) / steps));
+
+    for (let i = 0; i < iterations; i++) {
+        result *= Math.pow(2, Math.floor((exponent + i) / iterations));
     }
+
     return result;
 }
